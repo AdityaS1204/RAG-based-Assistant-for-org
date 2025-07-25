5e4af1d08a4b423bdd0cc0d70e6dc48e8062fe86
Aditya <adityas.singh.tech@gmail.com>
2025-07-26 00:22:23 +0530

    fronted intial commit

---
+++ Frontend/src/Routes/AppRouter.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import { createBrowserRouter } from 'react-router-dom'
+const AppRouter = () => {
+  return (
+    <div>AppRouter</div>
+  )
+}
+
+export default AppRouter
