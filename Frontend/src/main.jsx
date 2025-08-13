b5f141f18acc8c9ad669578972b1a012dd264754
Aditya <adityas.singh.tech@gmail.com>
2025-08-14 01:11:40 +0530

    feat: add router for routing between pages

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,10 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { RouterProvider } from 'react-router-dom'
+import AppRouter from './Routes/AppRouter.jsx'
 import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <RouterProvider router={AppRouter}/>
+  </StrictMode>
 )
