dc7fff8ccefadb071fbfb167dbd7e31742d64294
Aditya <adityas.singh.tech@gmail.com>
2025-08-28 13:27:16 +0530

    feat: create pie chart component to display stats on dashboard

---
+++ Frontend/src/components/Charts/Piechart.jsx
@@ -0,0 +1,86 @@
+import { Pie, PieChart, ResponsiveContainer, Sector} from 'recharts';
+
+const data = [
+    { name: 'HOD', value: 8 },
+    { name: 'Students', value: 300 },
+    { name: 'Faculty', value: 70 },
+    { name: 'Staff', value: 12 },
+];
+
+const renderActiveShape = ({
+    cx,
+    cy,
+    midAngle,
+    innerRadius,
+    outerRadius,
+    startAngle,
+    endAngle,
+    fill,
+    payload,
+    percent,
+    value,
+}) => {
+    const RADIAN = Math.PI / 180;
+    const sin = Math.sin(-RADIAN * (midAngle ?? 1));
+    const cos = Math.cos(-RADIAN * (midAngle ?? 1));
+    const sx = (cx ?? 0) + ((outerRadius ?? 0) + 10) * cos;
+    const sy = (cy ?? 0) + ((outerRadius ?? 0) + 10) * sin;
+    const mx = (cx ?? 0) + ((outerRadius ?? 0) + 30) * cos;
+    const my = (cy ?? 0) + ((outerRadius ?? 0) + 30) * sin;
+    const ex = mx + (cos >= 0 ? 1 : -1) * 22;
+    const ey = my;
+    const textAnchor = cos >= 0 ? 'start' : 'end';
+
+    return (
+        <g>
+            <text x={cx} y={cy} dy={8} textAnchor="middle" fill={'#ffffff'}>
+                {payload.name}
+            </text>
+            <Sector
+                cx={cx}
+                cy={cy}
+                innerRadius={innerRadius}
+                outerRadius={outerRadius}
+                startAngle={startAngle}
+                endAngle={endAngle}
+                fill={'#FFD93D'}
+            />
+            <Sector
+                cx={cx}
+                cy={cy}
+                startAngle={startAngle}
+                endAngle={endAngle}
+                innerRadius={(outerRadius ?? 0) + 6}
+                outerRadius={(outerRadius ?? 0) + 10}
+                fill={fill}
+            />
+            <path d={`M${sx},${sy}L${mx},${my}L${ex},${ey}`} stroke={fill} fill="none" />
+            <circle cx={ex} cy={ey} r={2} fill={fill} stroke="none" />
+            <text x={ex + (cos >= 0 ? 1 : -1) * 12} y={ey} textAnchor={textAnchor} fill="#ffffff">{`${value}`}</text>
+            <text x={ex + (cos >= 0 ? 1 : -1) * 12} y={ey} dy={18} textAnchor={textAnchor} fill="#ffffff">
+                {`(Rate ${((percent ?? 1) * 100).toFixed(2)}%)`}
+            </text>
+        </g>
+    );
+};
+
+const Piechart = () => {
+    return (
+        <ResponsiveContainer width="100%" height="100%" className='w-full h-full'>
+            <PieChart width={400} height={400}>
+                <Pie
+                    activeShape={renderActiveShape}
+                    data={data}
+                    cx="50%"
+                    cy="50%"
+                    innerRadius={60}
+                    outerRadius={80}
+                    fill="#FF9A00"
+                    dataKey="value"
+                />
+            </PieChart>
+        </ResponsiveContainer>
+    )
+}
+
+export default Piechart
